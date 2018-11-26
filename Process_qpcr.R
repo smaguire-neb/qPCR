12bc7fabebf4de8b635e941fb80114588acf222b
Sean Maguire <smaguire@neb.com>
2018-11-26 16:18:20 -0500

    Add datafiles, made process functions mapped with tidyr

--- Process_qpcr.R
+++ Process_qpcr.R
@@ -4,34 +4,12 @@
 library(dplyr)
 library(readxl)
 library(stringr)
-setwd("~/Desktop/qpcr")
+library(tidyr)
+library(purrr)
 
-layout1<-read_excel("layout-plate1.xlsx")
-base.dir<-"~/Desktop/qpcr/"
+setwd("~/Desktop/qpcr/Qpcr code/data")
 
-process.plate<-function(dir,plate.type = c("plate1","plate2")){
-  dir.i<-paste0(base.dir,dir)
-  cq.file<-list.files(dir.i,pattern="Quantification Cq Results -reformatted") %>%
-    .[!str_detect(.,"~")] %>%
-    paste0(base.dir,dir,"/",.)
-  melt.file<-list.files(dir.i,pattern="Melt Curve Summary -reformatted")  %>%
-    .[!str_detect(.,"~")] %>%
-    paste0(base.dir,dir,"/",.)
-  base.name<-list.files(dir.i,pattern="Quantification Cq Results") %>%
-    .[!str_detect(.,"~")] %>%
-    str_split(.,pattern=" - ",simplify = T) %>%
-    .[1,1]
-  cq.data<-read_excel(cq.file) %>%
-    select(Well,Cq)
-  melt.data<-read_excel(melt.file) %>%
-    group_by(Well) %>%
-    mutate(count=n(),
-           melt.flag=ifelse(count == 1,F,T)) %>%
-    mutate(melt.temp = suppressWarnings(as.numeric(`Melt Temp`))) %>%
-    group_by(melt.flag,Well) %>%
-    summarise(melt.temp=mean(melt.temp)) %>%
-    as.data.frame()
-  quant<-left_join(cq.data,melt.data)
+process.plate<-function(data,plate.type){
   if(plate.type == "plate1"){
     layout <- read_excel("layout-plate1.xlsx") %>%
       mutate(Well = well) %>%
@@ -41,33 +19,34 @@ process.plate<-function(dir,plate.type = c("plate1","plate2")){
       mutate(Well = well) %>%
       select(miRname,Well,sequence)
   }
-  full_join(quant,layout)
+  full_join(data,layout,by="Well") %>%
+    mutate(Cq = as.numeric(as.character(Cq)),
+           Melt.Temperature = as.numeric(as.character(Melt.Temperature))) %>%
+    mutate(flag = ifelse((Cq < 37) | (!(is.na(Melt.Temperature) & Cq > 35)),F,T))
 }
 
-results1<-process.plate("11-5-18","plate1")
-results2<-process.plate("11-12-18-plate1-rep2","plate1")
-r1<-
-  results1 %>%
-  filter( Cq < 37) %>%
-  filter(!(is.na(melt.temp) & Cq > 35))
+files<-list.files(pattern=".txt")
+data<-
+as_tibble(str_split(files,"-",simplify = T)) %>%
+  select(-V3) %>%
+  rename(plate.type = V1, replicate = V2) %>%
+  mutate(file.names = files) %>%
+  group_by(plate.type,replicate) %>%
+  nest(.key="file.name") %>%
+  mutate(raw.data = map(.$file.name, function(x) unlist(x) %>%
+                      read.delim(.,skip=19))) %>%
+  mutate(processed.data = map2(.$raw.data,.$plate.type, process.plate))
 
-r2<-
-  results2 %>%
-  filter( Cq < 37) %>%
-  filter(!(is.na(melt.temp) & Cq > 35))
+names(data$processed.data)<-paste(data$plate.type,data$replicate,sep="-")
+long.data<-bind_rows(data$processed.data,.id="file")
+long.data %>%
+  mutate(plate = str_split(file,"-",simplify = T)[,1],
+         rep = str_split(file,"-",simplify = T)[,2])
+
+left_join(data$processed.data[[2]],data$processed.data[[3]],by="Well") %>%
+  ggplot(aes(x=Cq.x,y=Cq.y))+geom_point()
 
 
-left_join(r1,r2,by=c("Well","miRname","sequence")) %>%
-  ggplot(aes(x=Cq.x,y=Cq.y))+geom_point() + xlab("Cq rep1") + 
-  ylab("Cq rep 2")
-
-qpcr.data.plate1<-bind_rows(results1,results2) %>%
-  group_by(miRname) %>%
-  mutate(mean.cq = mean(Cq),
-         sd.cq=sd(Cq),
-         cv=sd.cq/mean.cq) %>%
-  filter(cv <= 0.05,
-         mean.cq<37)
 #Take a list of processed plates and convert them to qpcr data
 #check the CVs. If they are bad eventually figure out some way to toss the outliers. 
 #Toss the outliers if they fail the CV check then find the mean of all replicates, subtract the mean, toss the one with the highest abs. value. 
