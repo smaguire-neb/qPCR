1e425566510b3cbab66db700f54095eac3ef55a8
Sean Maguire <smaguire@neb.com>
2018-12-13 19:12:24 -0500

    Fixed the names and I made the plots

--- Process_qpcr.R
+++ Process_qpcr.R
@@ -73,6 +73,7 @@ mutate(long.data,plate = str_split(file,"-",simplify = T)[,1],
   left_join(long.data,.) %>%
   mutate(corrected.cq = Cq-ipc.cf)
 
+corrected.summ.data<-
 corrected.long.data %>%
   mutate(plate = str_split(file,"-",simplify = T)[,1],
          rep = str_split(file,"-",simplify = T)[,2]) %>%
@@ -85,8 +86,7 @@ corrected.long.data %>%
   summarise(mean.corrected.cq = mean(corrected.cq),
             sd.corrected.cq = sd(corrected.cq),
             cv.corrected.cq = (sd.corrected.cq/mean.corrected.cq)*100) %>%
-  filter(mean.corrected.cq < 37, cv.corrected.cq < 5) %>%
-  ggplot(aes(x=mean.corrected.cq))+geom_density()
+  filter(mean.corrected.cq < 37, cv.corrected.cq < 5)
   
 
 #filter them so the just contain flag, cq, well, name and sequence
@@ -100,8 +100,72 @@ corrected.long.data %>%
 #check the CVs. If they are bad eventually figure out some way to toss the outliers. 
 #Toss the outliers if they fail the CV check then find the mean of all replicates, subtract the mean, toss the one with the highest abs. value. 
 
+#splint.full.data<-read.delim("splint.full.tabular",header=F,skip=12)
+splint.count.data<-
+splint.full.data %>%
+  group_by(V3) %>%
+  summarise(splint.count=n()) %>%
+  rename("miRname" = V3) %>%
+  mutate(miRname = as.character(miRname))
 
+#NN.full.data<-read.delim("NN.full.tabular",header=F,skip=12)
+NN.count.data<-
+  NN.full.data %>%
+  group_by(V3) %>%
+  summarise(NN.count=n()) %>%
+  rename("miRname" = V3) %>%
+  mutate(miRname = as.character(miRname))
 
+layout1 <- read_excel("layout-plate1.xlsx")
+layout2 <- read_excel("layout-plate2.xlsx")
+
+mirNames<-
+rbind(layout1,layout2) %>%
+  filter(sequence != "",
+         !is.na(sequence))
+
+mirNames$miRname
+
+length(intersect(as.character(splint.count.data$miRname),mirNames$miRname))
+#464 present in splint dataset and miRnome dataset 
+length(intersect(as.character(NN.count.data$miRname),mirNames$miRname))
+#437
+
+joined.data<-
+inner_join(corrected.summ.data,splint.count.data) %>%
+  inner_join(.,NN.count.data) %>%
+  mutate(log.cq = log(mean.corrected.cq),
+         log.splint.count = log(splint.count),
+         log.NN.count = log(NN.count))
+
+nrow(joined.data)
+
+#690 detected by qpcr
+# of those 452 were detected by Splint
+# and 428 were detected by NN
+
+454-428
+nrow(inner_join(corrected.summ.data,splint.count.data))
+nrow(inner_join(corrected.summ.data,NN.count.data))
+
+splint.qpcr.seq<-
+ggplot(joined.data,aes(x=log.cq,y=log.splint.count))+geom_point() + 
+  geom_smooth(method="lm") +
+  xlab("qPCR Cq (Log Scale)") +
+  ylab("RNAseq Read Count (Log Scale)") +
+  ggtitle("Splint Method")
+NN.qpcr.seq<-
+ggplot(joined.data,aes(x=log.cq,y=log.NN.count))+geom_point() + 
+  geom_smooth(method="lm") +
+  xlab("qPCR Cq (Log Scale)") +
+  ylab("RNAseq Read Count (Log Scale)") +
+  ggtitle("NEBNext")
+
+summary(lm(log.cq~log.splint.count,data=joined.data))
+summary(lm(log.cq~log.NN.count,data=joined.data))
+
+save_plot("NNseqQ.png",NN.qpcr.seq)
+save_plot("SplintSeqQ.png",splint.qpcr.seq)
 
 # convert sequence names --------------------------------------------------
 
@@ -123,11 +187,35 @@ writeFasta<-function(data, filename){
 }
 
 full.layout<-bind_rows(layout1,layout2) %>% 
-  select(miRname,sequence) %>%
   filter(!(is.na(sequence))) %>%
-  dplyr::rename("name"="miRname","seq"="sequence")
+  mutate(dna.sequence=str_replace_all(sequence,"U","T")) %>%
+  select(miRname,dna.sequence) %>%
+  dplyr::rename("name"="miRname","seq"="dna.sequence")
 
 writeFasta(full.layout,"miRnome.fasta")
 
+qpcr.reads<-read.delim("qpcr_seqs2ReadsPerGene.out.tab",head=F)
+nrow(qpcr.reads)
+head(qpcr.reads)
 
+qpcr.reads.filtered<-filter(qpcr.reads,!(V2 ==0 & V3 == 0 & V4 == 0))
+sum(qpcr.reads.filtered[-1:-4,2])
 
+full.layout
+
+gtf<-rtracklayer::import("~/Desktop/STAR pipeline/miRNAannotation.gtf")
+gtf_df=as_data_frame(gtf) #%>%
+  #filter(type=="gene") %>%
+  #dplyr::select(gene_id,gene_name)
+test<-filter(gtf_df,seqnames == "chr5",
+       end > 168768211,
+       start < 168768211)
+test$transcript_name
+filter(qpcr.reads.filtered,V1=="ENSG00000207739.1")
+ENSG00000207630.1
+ENSG00000207703.1
+ENSG00000207603.1
+
+ENST00000384898.1
+ENST00000384970.1
+ENST00000384871.1
